a1767e227d9ec8fe84a9b89c079a3ee5fab893e3
juliana15paak <112185575+juliana15paak@users.noreply.github.com>
2022-11-04 08:49:03 -0300

    Add files via upload

---
+++ multiplodez.java
@@ -0,0 +1,12 @@
+package teams;
+public class multiplodez {
+	public static void main (String args[]) {
+		int i;
+		for (i=1; i<=100; i++) {
+			System.out.println(i);
+			if (i==10 | i==20 | i==30 | i==40 | i==50 | i==60 | i==70 | i==80 | i==90 | i==100){
+				System.out.println("Múltiplo de 10");
+			}
+		}
+	}
+}
