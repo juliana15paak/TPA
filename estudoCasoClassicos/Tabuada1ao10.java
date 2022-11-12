faae3c29aab4644b5e6984d76646fcc1c4400c2f
juliana15paak <112185575+juliana15paak@users.noreply.github.com>
2022-11-12 08:51:59 -0300

    Add files via upload

---
+++ estudoCasoClassicos/Tabuada1ao10.java
@@ -0,0 +1,12 @@
+package teams;
+public class Tabuada1ao10 {
+	public static void main (String args[]) {
+		int f, i, res;
+		for (f=1; f<=10; f++) {
+			for (i=1; i<=10; i++) {
+				res=f*i;
+				System.out.println(f+" x "+i+" = "+res);
+			}
+		}
+	}
+}
