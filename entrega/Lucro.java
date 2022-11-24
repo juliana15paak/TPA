055344280dbaa0b849773610b66a53831cf875f5
juliana15paak <112185575+juliana15paak@users.noreply.github.com>
2022-11-24 20:28:38 -0300

    Update Lucro.java

--- entrega/Lucro.java
+++ entrega/Lucro.java
@@ -5,7 +5,7 @@ public class Lucro {
 		int i;
 		double mk, preco, pv;
 		i=1;
-		while (i==1) {
+		do {
 			System.out.println("Digite o pre�o: ");
 			preco = in.nextDouble();
 			System.out.println("Digite a margem de lucro em porcentagem: %");
@@ -14,7 +14,7 @@ public class Lucro {
 			System.out.println("Preco de venda �: "+pv);
 			System.out.println("Deseja continuar a execu��o do programa? Digite ''1'' para SIM, e ''2'' para N�O.");
 			i = in.nextInt();
-		}
+		}while (i==1);
 		in.close();
 	}
 }
