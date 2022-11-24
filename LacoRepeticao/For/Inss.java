d9cd770e40fb518242226aeb535bc493aafb2ad2
juliana15paak <112185575+juliana15paak@users.noreply.github.com>
2022-11-24 20:31:39 -0300

    Add files via upload

---
+++ LacoRepeticao/For/Inss.java
@@ -0,0 +1,23 @@
+﻿import java.util.Scanner;
+public class Inss {
+	public static void main(String[]args) {
+		Scanner in = new Scanner (System.in);
+		int i;
+		double s, desc, salario;
+		for(i=1;i<=4;i++){
+			System.out.println("Digite o salário: ");
+			s = in.nextDouble();
+			if (s>=2000){
+				desc = s*11/100;
+				salario = s-desc;
+				System.out.println(salario);
+			}
+			else {
+				desc = s*8.5/100;
+				salario = s-desc;			
+				System.out.println(salario);
+			}
+		}
+		in.close();
+	}
+}
