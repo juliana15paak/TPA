569ebe6229abf9e43b3ce455ef46b0039a5885ca
juliana15paak <112185575+juliana15paak@users.noreply.github.com>
2022-11-24 20:29:14 -0300

    Update Tabuada.java

--- entrega/Tabuada.java
+++ entrega/Tabuada.java
@@ -3,13 +3,11 @@ public class Tabuada {
 	public static void main(String[]args) {
 		Scanner in = new Scanner (System.in);
 		int i, n, res;
-		i=1;
-		while (i<=10) {
+		for(i=1; i<=10; i++){
 			System.out.println("Digite um número inteiro: ");
 			n = in.nextInt();
 			res = n*i;
 			System.out.println(n+"x"+i+"="+res);
-			i++;
 		}
 		in.close();
 	}
